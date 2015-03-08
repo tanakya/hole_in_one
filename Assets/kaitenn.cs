4e80edaa5ed89c317b17fdd6545a5653dfca21d4
unknown <sax.putin@gmail.com>
2015-03-08 17:41:51 +0900

    地面を回転

---
+++ Assets/kaitenn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kaitenn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(Input.GetKeyDown("up")){
+		  }
+		if(Input.GetKeyDown("down")){
+		}
+		// 水平方向の入力量(-1〜1)
+		float horizontal = Input.GetAxis("Horizontal");
+		
+		// 垂直方向の入力量(-1〜1)
+		float vertical = Input.GetAxis("Vertical");
+		ver rotation =Quaternion.Euler(y,x,0)
+
+			transform.RotateAround(new Vector3 (1, 0, 1), transform.up, 45*Time.deltaTime);
+	}
+	
+	}
+
+}
